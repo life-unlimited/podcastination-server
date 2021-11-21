82761810f7fa23a7ccc71cd06ddf33b4a9907a36
Lennart Altenhof <l.f.altenhof@gmail.com>
2021-11-21 13:05:05 +0100

    fix: add missing itunes:category tag to podcast xml

--- podcast_xml/file.go
+++ podcast_xml/file.go
@@ -14,21 +14,21 @@ type PodcastXML struct {
 }
 
 type channel struct {
-	Title            string           `xml:"title"`
-	Link             string           `xml:"link"`
-	Language         string           `xml:"language"`
-	AtomLink         atomLink         `xml:"atom:link"`
-	Copyright        string           `xml:"copyright"`
-	ITunesSubtitle   string           `xml:"itunes:subtitle"`
-	ITunesAuthor     string           `xml:"itunes:author"`
-	ITunesSummary    string           `xml:"itunes:summary"`
-	ITunesKeywords   string           `xml:"itunes:keywords"`
-	Description      string           `xml:"description"`
-	ITunesOwner      iTunesOwner      `xml:"itunes:owner"`
-	Image            image            `xml:"image"`
-	ITunesImage      iTunesImage      `xml:"itunes:image"`
-	ITunesCategories []iTunesCategory `xml:"itunes:category"`
-	Items            []item           `xml:"item"`
+	Title          string         `xml:"title"`
+	Link           string         `xml:"link"`
+	Language       string         `xml:"language"`
+	AtomLink       atomLink       `xml:"atom:link"`
+	Copyright      string         `xml:"copyright"`
+	ITunesSubtitle string         `xml:"itunes:subtitle"`
+	ITunesAuthor   string         `xml:"itunes:author"`
+	ITunesSummary  string         `xml:"itunes:summary"`
+	ITunesKeywords string         `xml:"itunes:keywords"`
+	Description    string         `xml:"description"`
+	ITunesOwner    iTunesOwner    `xml:"itunes:owner"`
+	Image          image          `xml:"image"`
+	ITunesImage    iTunesImage    `xml:"itunes:image"`
+	ITunesCategory iTunesCategory `xml:"itunes:category"`
+	Items          []item         `xml:"item"`
 }
 
 type atomLink struct {
@@ -51,7 +51,8 @@ type iTunesImage struct {
 }
 
 type iTunesCategory struct {
-	Text string `xml:"text,attr"`
+	Text          string           `xml:"text,attr"`
+	SubCategories []iTunesCategory `xml:"itunes:category"`
 }
 
 type item struct {
